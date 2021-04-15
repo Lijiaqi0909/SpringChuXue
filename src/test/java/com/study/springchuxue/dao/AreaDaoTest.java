8c66fb8a4c4f7c87e1f7b69129efa5ba5059c736
Li jq <17600260156@163.com>
2021-04-15 20:25:39 +0800

    第二次提交

--- src/test/java/com/study/springchuxue/dao/AreaDaoTest.java
+++ src/test/java/com/study/springchuxue/dao/AreaDaoTest.java
@@ -1,28 +1,29 @@
 package com.study.springchuxue.dao;
 
 import com.study.springchuxue.entity.Area;
-import org.junit.Assert;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class AreaDaoTest {
 
     @Autowired
-    private  AreaDao areaDao;
+    public AreaDao areaDao;
     @Test
     void querryArea() {
         List<Area> areas = areaDao.querryArea();
-        assertEquals(2,areas.size());
+        Assertions.assertEquals(2,areas.size());
 
     }
 
