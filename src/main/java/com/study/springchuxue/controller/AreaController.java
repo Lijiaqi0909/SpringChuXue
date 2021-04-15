16d523389e4034ffc19d692496cabbccf0c84d57
Li jq <17600260156@163.com>
2021-04-15 21:25:38 +0800

    controller类创建

---
+++ src/main/java/com/study/springchuxue/controller/AreaController.java
@@ -0,0 +1,15 @@
+package com.study.springchuxue.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description controller类
+ * @Author Li jq
+ * @Date 2021/4/15 21:23
+ **/
+@RestController
+@RequestMapping(value = "/Area")
+public class AreaController {
+
+}
